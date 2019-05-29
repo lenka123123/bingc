1b089590f79213c6836143029e18593a04c4f779
address_hzw <13902276741@163.com>
2019-05-29 18:11:09 +0800

    提交

--- app/src/main/java/bcms/monite/cn/bingchen/MainActivity.java
+++ app/src/main/java/bcms/monite/cn/bingchen/MainActivity.java
@@ -6,13 +6,44 @@ import android.support.v7.app.AppCompatActivity;
 import com.orhanobut.logger.Logger;
 import com.yanzhenjie.sofia.Sofia;
 
+import bcms.monite.cn.bingchen.utils.AESUtils;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * RSA  公钥  rsaPublicKey
+     * AES  密实  aesKey
+     * <p>
+     * 加密前 json
+     * aesKey 对json进行加密   加密后encruyptData
+     * <p>
+     * rsaPublicKey对 aesKey进行加密  encryptAesKey
+     * <p>
+     * encryptAesKey 为请求头 将encruyptData 传输出去
+     */
+    private String aesKey;
+    public final static String ENCODING = "UTF-8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Sofia.with(this).statusBarBackground(R.color.colorAccent);
-        Logger.i("信息","hehehe");
+        // 自定义json
+        String request = "123456";
+        try {
+            aesKey = AESUtils.getAESKey();//
+            //加密后json
+            String encruyptData = AESUtils.aesEncrypt(request, ENCODING, "ECB", aesKey, "");
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+
 }
